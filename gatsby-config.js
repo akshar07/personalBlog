dd0dd5f7d5e8233f2ebc2fe68551537026cae9ae
akshart <MYEMAIL>
2020-02-26 17:58:28 -0800

    add plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,22 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-responsive-image`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ]
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/media`,
